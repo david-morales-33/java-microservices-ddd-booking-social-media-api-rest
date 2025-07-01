b0729d3d282d9878922b6fe6303dddbb979817c9
david-morales-33 <davidmorales33.97@gmail.com>
2025-07-01 18:16:34 -0500

    Add: repository <UserProfileRepository>; service:<microservice-profile>

---
+++ microservice-profile/src/main/java/com/dmx/profile/user_profile/domain/UserProfileRepository.java
@@ -0,0 +1,14 @@
+package com.dmx.profile.user_profile.domain;
+
+import com.dmx.profile.shared.domain.criteria.Criteria;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserProfileRepository {
+    void save(UserProfile profile);
+
+    Optional<UserProfile> find(UserProfileId id);
+
+    List<UserProfile> searchByCriteria(Criteria criteria);
+}
