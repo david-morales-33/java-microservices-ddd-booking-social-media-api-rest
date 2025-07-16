99b893675e6812d8f2af90e74a04662db1a27ec2
david-morales-33 <davidmorales33.97@gmail.com>
2025-07-16 15:22:34 -0500

    ADD: microservice: <microservice-social-graph>

---
+++ microservice-social-graph/src/main/java/com/dmx/server/MicroserviceSocialGraphApplication.java
@@ -0,0 +1,13 @@
+package com.dmx.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSocialGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSocialGraphApplication.class, args);
+	}
+
+}
---
+++ microservice-social-graph/src/test/java/com/dmx/server/MicroserviceSocialGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmx.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSocialGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
