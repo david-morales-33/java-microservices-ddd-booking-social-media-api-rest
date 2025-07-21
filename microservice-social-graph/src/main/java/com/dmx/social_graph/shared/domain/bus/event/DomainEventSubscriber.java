4ecf3800e9186ef79d04d6460412a07c92d3bad0
david-morales-33 <davidmorales33.97@gmail.com>
2025-07-21 18:11:32 -0500

    Add: shared domain objects; <microservice-social-graph>

---
+++ microservice-social-graph/src/main/java/com/dmx/social_graph/shared/domain/bus/event/DomainEventSubscriber.java
@@ -0,0 +1,10 @@
+package com.dmx.profile.shared.domain.bus.event;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
+public @interface DomainEventSubscriber {
+    Class<? extends DomainEvent>[] value();
+}
